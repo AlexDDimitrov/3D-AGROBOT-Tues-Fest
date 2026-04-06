bc77c7a6e3fb180ec68af5bda70b80093be66e2f
TeodorDDimitrov <teo.dim.dimitrov@gmail.com>
2026-04-06 19:18:15 +0300

    Enhance motor control and add servo functionality and final push for arduino
    Added servo control and improved motor functions.

--- Arduino/Ardoino/Ardoino.ino
+++ Arduino/Ardoino/Ardoino.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
+#include <Servo.h>
 
 #define MPU_ADDR      0x68
 #define PWR_MGMT_1    0x6B
 #define ACCEL_CONFIG  0x1C
 #define ACCEL_YOUT_H  0x3D
+#define GYRO_ZOUT_H   0x47
 
 float targetDistance = 0;
 
@@ -12,12 +14,15 @@ float AcselY_Filter = 0;
 double velocityY      = 0.0;
 double distanceY      = 0.0;
 float biasY = 0.0;
+
+float gyroZ_bias = 0.0;
+float currentAngle = 0.0;
+
 unsigned long t_now = 0;
 unsigned long t_then = 0;
 
 unsigned long moveStartTime = 0;
 
-// Hardware Pins
 #define AIN1_R 22
 #define AIN2_R 23
 #define PWMA_R 44
@@ -32,45 +37,61 @@ unsigned long moveStartTime = 0;
 #define BIN2_L 29
 #define PWMB_L 2
 
+Servo servo;
+#define SERVO_PIN 7
+int servoMoveCount = 0;
+
 void rightForward(int speed) {
-  digitalWrite(AIN1_R, HIGH); digitalWrite(AIN2_R, LOW);
+  digitalWrite(AIN1_R, HIGH); 
+  digitalWrite(AIN2_R, LOW);
   analogWrite(PWMA_R, speed);
-  digitalWrite(BIN1_R, HIGH); digitalWrite(BIN2_R, LOW);
+  digitalWrite(BIN1_R, HIGH); 
+  digitalWrite(BIN2_R, LOW);
   analogWrite(PWMB_R, speed);
 }
 
 void rightBackward(int speed) {
-  digitalWrite(AIN1_R, LOW); digitalWrite(AIN2_R, HIGH);
+  digitalWrite(AIN1_R, LOW); 
+  digitalWrite(AIN2_R, HIGH);
   analogWrite(PWMA_R, speed);
-  digitalWrite(BIN1_R, LOW); digitalWrite(BIN2_R, HIGH);
+  digitalWrite(BIN1_R, LOW); 
+  digitalWrite(BIN2_R, HIGH);
   analogWrite(PWMB_R, speed);
 }
 
 void leftForward(int speed) {
-  digitalWrite(AIN1_L, HIGH); digitalWrite(AIN2_L, LOW);
+  digitalWrite(AIN1_L, HIGH); 
+  digitalWrite(AIN2_L, LOW);
   analogWrite(PWMA_L, speed);
-  digitalWrite(BIN1_L, HIGH); digitalWrite(BIN2_L, LOW);
+  digitalWrite(BIN1_L, HIGH); 
+  digitalWrite(BIN2_L, LOW);
   analogWrite(PWMB_L, speed);
 }
 
 void leftBackward(int speed) {
-  digitalWrite(AIN1_L, LOW); digitalWrite(AIN2_L, HIGH);
+  digitalWrite(AIN1_L, LOW);
+  digitalWrite(AIN2_L, HIGH);
   analogWrite(PWMA_L, speed);
-  digitalWrite(BIN1_L, LOW); digitalWrite(BIN2_L, HIGH);
+  digitalWrite(BIN1_L, LOW); 
+  digitalWrite(BIN2_L, HIGH);
   analogWrite(PWMB_L, speed);
 }
 
 void stopLMotors() {
-  digitalWrite(AIN1_L, LOW); digitalWrite(AIN2_L, LOW);
+  digitalWrite(AIN1_L, LOW); 
+  digitalWrite(AIN2_L, LOW);
   analogWrite(PWMA_L, 0);
-  digitalWrite(BIN1_L, LOW); digitalWrite(BIN2_L, LOW);
+  digitalWrite(BIN1_L, LOW); 
+  digitalWrite(BIN2_L, LOW);
   analogWrite(PWMB_L, 0);
 }
 
 void stopRMotors() {
-  digitalWrite(AIN1_R, LOW); digitalWrite(AIN2_R, LOW);
+  digitalWrite(AIN1_R, LOW); 
+  digitalWrite(AIN2_R, LOW);
   analogWrite(PWMA_R, 0);
-  digitalWrite(BIN1_R, LOW); digitalWrite(BIN2_R, LOW);
+  digitalWrite(BIN1_R, LOW); 
+  digitalWrite(BIN2_R, LOW);
   analogWrite(PWMB_R, 0);
 }
 
@@ -113,8 +134,8 @@ bool mpuInit() {
 
 void i2cRecover() {
    Wire.end();
-   pinMode(20, INPUT_PULLUP); // SDA на Mega = pin 20
-   pinMode(21, OUTPUT);       // SCL на Mega = pin 21
+   pinMode(20, INPUT_PULLUP); 
+   pinMode(21, OUTPUT);       
    for (int i = 0; i < 20; i++) {
      digitalWrite(21, LOW);
      delayMicroseconds(10);
@@ -143,7 +164,20 @@ float readAccelY() {
    return accel_cm_s2;
 }
 
-  /*This is for debugging from Arduinos terminal
+float readGyroZ() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(GYRO_ZOUT_H);
+  if (Wire.endTransmission(false) != 0) {
+    i2cRecover();
+    return 0.0;
+  }
+  Wire.requestFrom(MPU_ADDR, 2, true);
+  if (Wire.available() < 2) return 0.0;
+  int16_t raw = (Wire.read() << 8) | Wire.read();
+  return (float)raw / 131.0;
+}
+/*DEBUGGING FROM SERIAL MONITOR:
+String readIncomingCommand() {
   String str = "";
 
   if (Serial.available()) {
@@ -169,49 +203,62 @@ float readAccelY() {
   float val = numStr.toFloat();
   if (val > 0) return String(val, 2);
 
-  return "";*/
+  return "";
+}*/
 
+//FROM ESP32:
 String readIncomingCommand() {
   String str = "";
-
   if (Serial1.available()) {
     str = Serial1.readStringUntil('\n');
   } else {
     return "";
   }
-
   str.trim();
   if (str.length() == 0) return "";
-
   if (str.equalsIgnoreCase("L")) return "left";
   if (str.equalsIgnoreCase("R")) return "right";
-
   String numStr = str;
   if (str.charAt(0) == 'M' || str.charAt(0) == 'm') {
     numStr = str.substring(1);
     numStr.trim();
   }
-
   float val = numStr.toFloat();
   if (val > 0) return String(val, 2);
-
   return "";
 }
 
 void calibrate() {
-  Serial.print("Calibrating");
+  Serial.print("Calibrating...");
   biasY = 0.0;
+  gyroZ_bias = 0.0;
   for (int i = 0; i < 1000; i++) {
     biasY += readAccelY();
+    gyroZ_bias += readGyroZ();
     if (i % 200 == 0) Serial.print(".");
   }
   biasY /= 1000.0;
-  Serial.print(" bias=");
-  Serial.print(biasY, 2);
-  Serial.println(" OK");
+  gyroZ_bias /= 1000.0;
+}
+
+void SetMotorDriversToOutput(){
+  pinMode(AIN1_R, OUTPUT);
+  pinMode(AIN2_R, OUTPUT);
+  pinMode(PWMA_R, OUTPUT);
+  pinMode(BIN1_R, OUTPUT);
+  pinMode(BIN2_R, OUTPUT);
+  pinMode(PWMB_R, OUTPUT);
+  pinMode(AIN1_L, OUTPUT);
+  pinMode(AIN2_L, OUTPUT);
+  pinMode(PWMA_L, OUTPUT);
+  pinMode(BIN1_L, OUTPUT);
+  pinMode(BIN2_L, OUTPUT);
+  pinMode(PWMB_L, OUTPUT);
 }
 
 void setup() {
+  servo.attach(SERVO_PIN);
+  servo.write(0);
   Serial.begin(115200);
   Serial1.begin(115200);
   Wire.begin();
@@ -221,97 +268,117 @@ void setup() {
   if (!mpuInit()) Serial.println("MPU6050 init FAILED!");
   else            Serial.println("MPU6050 OK");
 
-  pinMode(AIN1_R, OUTPUT); pinMode(AIN2_R, OUTPUT); pinMode(PWMA_R, OUTPUT);
-  pinMode(BIN1_R, OUTPUT); pinMode(BIN2_R, OUTPUT); pinMode(PWMB_R, OUTPUT);
-  pinMode(AIN1_L, OUTPUT); pinMode(AIN2_L, OUTPUT); pinMode(PWMA_L, OUTPUT);
-  pinMode(BIN1_L, OUTPUT); pinMode(BIN2_L, OUTPUT); pinMode(PWMB_L, OUTPUT);
+  SetMotorDriversToOutput();
 
   stopRobot();
-
   calibrate();
-
   t_then = micros();
 }
 
 double calkdist(){
   float accelY = readAccelY() - biasY;
   AcselY_Filter = AcselY_Filter + ALPHA * (accelY - AcselY_Filter);
-
-  if(AcselY_Filter<0.0) {
-    AcselY_Filter=0.0;
-  }
-
+  if(AcselY_Filter<0.0) AcselY_Filter=0.0;
+    
   t_now = micros();
-  float dt = (float)(t_now - t_then ) / (float)1000000.0;
-
-  t_then = t_now;
-
+  float dt = (float)(t_now - t_then ) / 1000000.0;
+  t_then = t_now;  
+  
   velocityY += (double)AcselY_Filter * dt;
   distanceY += velocityY * dt;
   return distanceY;
 }
 
-void zero_var(){
-  AcselY_Filter = 0;
-  velocityY = 0.0;
-  distanceY= 0.0;
-  t_now = 0;
-  t_then = 0;
-}
-
-
-static unsigned long lastPrint = 0;
-
-
 void loop() {
   String cmd = readIncomingCommand();
-
   delay(10);
 
+  t_now = micros();
+  float dt = (float)(t_now - t_then) / 1000000.0;
+  t_then = t_now;
+
+  float gz = readGyroZ() - gyroZ_bias;
+  if (abs(gz) > 0.1) {
+     currentAngle += gz * dt;
+  }
+
+  static unsigned long lastPrint = 0;
+  if (millis() - lastPrint > 100) {
+    lastPrint = millis();
+  }
+
   if (cmd == "left") {
-    Serial.println("Turning LEFT...");
-    stopRobot();
+    servoMoveCount++;
+    if (servoMoveCount >= 2) {
+      servoMoveCount = 0;
+      servo.write(180);
+    }
+
+    Serial.println("Turning LEFT");
+    currentAngle = 0.0;
+    t_then = micros();
     turnLeft(150);
-    delay(1100);
+
+    while (currentAngle < 90.0) {
+      //Debuging:
+      //Serial.println(currentAngle);
+      t_now = micros();
+      float turn_dt = (float)(t_now - t_then) / 1000000.0;
+      t_then = t_now;
+
+      float turn_gz = readGyroZ() - gyroZ_bias;
+      if (abs(turn_gz) > 0.1) {
+         currentAngle += turn_gz * turn_dt;
+      }
+    }
     stopRobot();
     Serial.println("Turn done.");
 
   } else if (cmd == "right") {
-    Serial.println("Turning RIGHT...");
-    stopRobot();
+    servoMoveCount++;
+    if (servoMoveCount >= 2) {
+      servoMoveCount = 0;
+      servo.write(0);
+    }
+
+    Serial.println("Turning RIGHT");
+    currentAngle = 0.0;
+    t_then = micros();
     turnRight(150);
-    delay(1100);
+
+    while (currentAngle > -90.0) {
+      //Debuging:
+      //Serial.println(currentAngle);
+      t_now = micros();
+      float turn_dt = (float)(t_now - t_then) / 1000000.0;
+      t_then = t_now;
+
+      float turn_gz = readGyroZ() - gyroZ_bias;
+      if (abs(turn_gz) > 0.1) {
+         currentAngle += turn_gz * turn_dt;
+      }
+    }
     stopRobot();
     Serial.println("Turn done.");
 
   } else if (cmd.length() > 0) {
     float val = cmd.toFloat();
     if (val > 0) {
-      targetDistance = val;
-      Serial.print("Target set: ");
-      Serial.print(val);
-      Serial.println(" cm — moving forward...");
-      calibrate();
-      zero_var();
-      moveForward(160);
-      t_then = micros();
-      lastPrint=millis();
-    }
-  }
+      float msPerCm = 25.0;
+      unsigned long duration = (unsigned long)(val * msPerCm);
+      //debuging:
+      //Serial.print("Moving ");
+      //Serial.print(val);
+      //Serial.print(" cm for ");
+      //Serial.print(duration);
+      //Serial.println(" ms");
 
-  if (targetDistance > 0) {
-    distanceY = calkdist();
-    if (distanceY >= targetDistance) {
+      moveForward(160);
+      delay(duration);
       stopRobot();
 
-      Serial.print(">> Destination reached! Total: ");
-      Serial.print(distanceY, 1);
-      Serial.println(" cm");
-
+      Serial.println("Destination reached!");
       Serial1.println("R");
-      calibrate();
-      zero_var();
-      targetDistance = -1.0;
     }
   }
 }
