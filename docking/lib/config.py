63e73c187b8939e7eb2ce29d61bd912f5c862595
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-04-18 09:46:15 +0300

    Added new link

--- docking/lib/config.py
+++ docking/lib/config.py
@@ -8,7 +8,7 @@ class Config:
 	ESP32_IP = os.getenv("ESP32_IP", "172.20.10.8")
 	ESP32_PORT = int(os.getenv("ESP32_PORT", "8080"))
 	CAMERA_PORT = int(os.getenv("CAMERA_PORT", "8888"))
-	API_URL = os.getenv("API_URL", "https://3d-agrobot-production.up.railway.app")
+	API_URL = os.getenv("API_URL", "https://3d-agrobot-tues-fest-production.up.railway.app/")
 	GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
 	TIMEOUT = 120.0
